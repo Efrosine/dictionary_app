3f1ca61578be87f49ab23908a34012ef4eeb3158
Rama <ramadhanipasuleri123@gmail.com>
2023-11-14 10:17:42 +0700

    po

--- lib/log_component/log_button.dart
+++ lib/log_component/log_button.dart
@@ -1,16 +1,17 @@
+import 'package:dictionary_app/Home_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class logButton extends StatefulWidget {
-  const logButton({super.key});
+ const logButton({super.key});
 
-  @override
-  State<logButton> createState() => _logButtonState();
+ @override
+ State<logButton> createState() => _logButtonState();
 }
 
 class _logButtonState extends State<logButton> {
-  @override
-  Widget build(BuildContext context) {
+ @override
+ Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(25.0),
       margin: EdgeInsets.symmetric(horizontal: 20),
@@ -18,16 +19,24 @@ class _logButtonState extends State<logButton> {
         color: Colors.black,
         borderRadius: BorderRadius.circular(8),
       ),
-      child: const Center(
-        child: Text(
-          "Sign in",
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontSize: 16
+      child: Center(
+        child: TextButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => HomePage()),
+            );
+          },
+          child: const Text(
+            "Sign in",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 16
+            ),
           ),
         ),
       ),
     );
-  }
+ }
 }
---
+++ lib/register_page.dart
@@ -0,0 +1,107 @@
+import 'package:dictionary_app/log_component/log_Textfield.dart';
+import 'package:dictionary_app/log_component/log_button.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+class Register extends StatefulWidget {
+  const Register({super.key});
+
+  @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  @override
+  Widget build(BuildContext context) {
+     return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+          child: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 40),
+            Image.asset('assets/images/logo.png', height: 100),
+            const SizedBox(height: 40),
+            Text(
+              'Welcome to dictionary',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 16,
+              ),
+            ),
+            const SizedBox(height: 25),
+            //user name
+            LogTextField(
+              hintText: 'Username',
+              secretText: false,
+            ),
+
+            const SizedBox(height: 10),
+            //password
+            LogTextField(
+              hintText: 'Passworld',
+              secretText: true,
+            ),
+            const SizedBox(height: 15),
+            Text(
+              'Forgot Passworld ?',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 16, color: Colors.grey),
+            ),
+            const SizedBox(height: 25),
+            logButton(),
+
+            const SizedBox(height: 25),
+            Row(
+              children: [
+                Expanded(
+                  child: Divider(
+                    thickness: 0.5,
+                    color: Colors.black,
+                  ),
+                ),
+                Text('Or Continue With'),
+                Expanded(
+                  child: Divider(
+                    thickness: 0.5,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 15),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/images/google.png',
+                  width: 90,
+                  height: 90,
+                ),
+                SizedBox(width: 15),
+                Image.asset(
+                  'assets/images/fb.png',
+                  width: 90,
+                  height: 90,
+                ),
+              ],
+            ),
+            const SizedBox(height: 15),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Already member?  '),
+                Text('Login',
+                style: TextStyle(
+                  color: Colors.blueAccent
+                ),)
+              ],
+            ),
+          ],
+        ),
+      )),
+    );
+  }
+}
