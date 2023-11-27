69ade29e83302e7c173980cca80c8dd12f2d8a0e
Rama <ramadhanipasuleri123@gmail.com>
2023-11-27 12:55:23 +0700

    pus

--- lib/page2.dart
+++ lib/page2.dart
@@ -11,26 +11,7 @@ class _Page2State extends State<Page2> {
  @override
  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        title: Text('Search', style: TextStyle(color: Colors.blue)),
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.black),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('Page 2'),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text('Search', style: TextStyle(color: Colors.blue)),
-          ),
-        ],
-      ),
+
     );
  }
 }
